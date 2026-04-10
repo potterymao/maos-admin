00433524bba617556b35d56b25bfa8b768b9f23c
hirosi912-g <hirosi912@gmail.com>
2026-04-10 17:30:42 +0800

    fix

--- app/stores/useDesignStore.ts
+++ app/stores/useDesignStore.ts
@@ -40,14 +40,15 @@ export const useDesignStore = defineStore("design", {
       const response = await GetPlates();
       // console.log("Loaded plates:", response.items);
       for (const item of response.items) {
-        const imageData = item.medias?.[0]?.images.source.url ? await GetImage(item.medias?.[0]?.images.source.url) : "";
+        // const imageData = item.medias?.[0]?.images.source.url ? await GetImage(item.medias?.[0]?.images.source.url) : "";
 
         this.plates.push({
           id: item.id,
           name_en: item.title_translations.en,
           name_zh: item.title_translations["zh-hant"],
           // image: item.medias?.[0]?.images.source.url || "",
-          image: imageData,
+          // image: imageData,
+          image: item.medias?.[0]?.images.source.url ? await GetImage(item.medias?.[0]?.images.source.url) : "";
           type: item.type,
           size: { width: 350, height: 350 },
           price: item.price.dollar || 300,
@@ -105,7 +106,7 @@ export const useDesignStore = defineStore("design", {
       const response = await GetPatterns();
       // console.log("Loaded patterns:", response.items);
       for (const item of response.items) {
-        const imageData = item.medias?.[0]?.images.source.url ? await GetImage(item.medias?.[0]?.images.source.url) : "";
+        const imageData = item.medias?.[0]?.images.source.url ? GetImage(item.medias?.[0]?.images.source.url) : "";
         
         this.patterns.push({
           id: item.id,
