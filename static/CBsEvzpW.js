cb36f915f36cab782dd07239035f76592a549166
hirosi912-g <hirosi912@gmail.com>
2026-05-07 18:08:46 +0800

    Updates

--- static/CBsEvzpW.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./C2eL0Omp.js";import"./BBsfkqJx.js";const r=e();function p({redirect:t}){if(r.firstPage)return t("/"+r.firstPage)}export{p as default};
---
+++ static/CqPGXSAU.js
@@ -0,0 +1,1 @@
+import{u as e}from"./CLJzWJ-x.js";import"./Dg3B86RE.js";const r=e();function p({redirect:t}){if(r.firstPage)return t("/"+r.firstPage)}export{p as default};
